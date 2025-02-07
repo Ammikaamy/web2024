4bf26e8571a61c93a9cd32c6551fbf5906ba582e
Ammikaamy <109019104+Ammikaamy@users.noreply.github.com>
2025-02-07 12:58:14 +0700

    Update work6.js

--- work6.js
+++ work6.js
@@ -27,14 +27,14 @@ class App extends React.Component {
       );
     }      
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "..",
-    appId: "...",
-    measurementId: "..."
-  };
+  apiKey: "AIzaSyDOHna1KebH_Yrv5q9WUEw7_zkfTpx1cBQ",
+  authDomain: "web2567-a5441.firebaseapp.com",
+  projectId: "web2567-a5441",
+  storageBucket: "web2567-a5441.firebasestorage.app",
+  messagingSenderId: "738533559337",
+  appId: "1:738533559337:web:7a5ecae9fe687c1960a038",
+  measurementId: "G-784M00GQML"
+};
 firebase.initializeApp(firebaseConfig);      
 const db = firebase.firestore();
 db.collection("students").get().then((querySnapshot) => {
